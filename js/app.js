0130dbf5ff3d66bf1f6c3d663e453fe41f0251f2
Saifur Rahman <srahman@drfirst.com>
2018-07-12 09:48:52 -0400

    working on modal

--- js/app.js
+++ js/app.js
@@ -3,162 +3,161 @@
  */
 
 var MemoryGame = {};
-const MATCH = "match";
-const OPEN = "open";
-const SHOW = "show";
-const MOVES = "moves";
-const RESTART = "restart";
-const CARD = "card";
-const DECK = "deck";
-const STAR_HALF_FULL = "fa-star-half-o";
-const STAR_FULL_EMPTY = "fa fa-star-o";
-const FULL_STAR = "fa-star";
+const MATCH = 'match';
+const OPEN = 'open';
+const SHOW = 'show';
+const MOVES = 'moves';
+const RESTART = 'restart';
+const CARD = 'card';
+const DECK = 'deck';
+const STAR_HALF_FULL = 'fa-star-half-o';
+const STAR_FULL_EMPTY = 'fa fa-star-o';
+const FULL_STAR = 'fa-star';
 
 MemoryGame.PLAY = {
-  initializeVariables: function() {
-    MemoryGame.pairedArray = [];
-    MemoryGame.timeoutInMs = 500;
-    MemoryGame.cards = document.querySelectorAll(`.${CARD}`);
-    MemoryGame.deck = document.getElementsByClassName(DECK)[0];
-  },
-  doPlay: function() {
-    MemoryGame.cards.forEach(currentElement => {
-      currentElement.addEventListener("click", function() {
-        if (currentElement.classList.contains(MATCH)) return;
-        if (currentElement.classList.contains(OPEN)) return;
+	initializeVariables: function() {
+		MemoryGame.pairedArray = [];
+		MemoryGame.timeoutInMs = 500;
+		MemoryGame.cards = document.querySelectorAll(`.${CARD}`);
+		MemoryGame.deck = document.getElementsByClassName(DECK)[0];
+	},
+	doPlay: function() {
+		MemoryGame.cards.forEach((currentElement) => {
+			currentElement.addEventListener('click', function() {
+				if (currentElement.classList.contains(MATCH)) return;
+				if (currentElement.classList.contains(OPEN)) return;
 
-        MemoryGame.pairedArray.push(currentElement);
+				MemoryGame.pairedArray.push(currentElement);
 
-        //step 1: save in an array that can flick and stay in the DOM
-        currentElement.classList.add(OPEN, SHOW);
-        //step 2: add `match` class to make it visible in UI
-        MemoryGame.DISPLAY.doMatch(currentElement);
-        MemoryGame.DISPLAY.showModelIfApplicable();
-        return;
-      });
-    });
-  },
-  trackMoves: function() {
-    if (MemoryGame.pairedArray.length === 2) {
-      let moves = document.getElementsByClassName(MOVES)[0].textContent;
-      document.getElementsByClassName(MOVES)[0].textContent = String(
-        parseInt(moves) + 1
-      );
-    }
-  },
-  reduceStar: function() {
-    let moves = document.getElementsByClassName(MOVES)[0].textContent;
-    let movesToInt = parseInt(moves);
-    let fullStars = document.getElementsByClassName(FULL_STAR)[0];
-    let halfStars = document.getElementsByClassName(STAR_HALF_FULL)[0];
-    if (movesToInt % 4 === 2 && fullStars != undefined) {
-      let elem = document.getElementsByClassName(FULL_STAR)[0];
-      elem.classList.remove(FULL_STAR);
-      elem.classList.add(STAR_HALF_FULL);
-    } else if (movesToInt % 8 === 2 && halfStars != undefined) {
-      let elem = document.getElementsByClassName(STAR_HALF_FULL)[0];
-      elem.classList.remove(STAR_HALF_FULL);
-    }
-  },
-  doRestartGame: function() {
-    document
-      .getElementsByClassName(RESTART)[0]
-      .addEventListener("click", function() {
-        restart();
-        resetMovesToZero();
-      });
+				//step 1: save in an array that can flick and stay in the DOM
+				currentElement.classList.add(OPEN, SHOW);
+				//step 2: add `match` class to make it visible in UI
+				MemoryGame.DISPLAY.doMatch(currentElement);
+				MemoryGame.DISPLAY.showModelIfApplicable();
+				return;
+			});
+		});
+	},
+	trackMoves: function() {
+		if (MemoryGame.pairedArray.length === 2) {
+			let moves = document.getElementsByClassName(MOVES)[0].textContent;
+			document.getElementsByClassName(MOVES)[0].textContent = String(parseInt(moves) + 1);
+		}
+	},
+	reduceStar: function() {
+		let moves = document.getElementsByClassName(MOVES)[0].textContent;
+		let movesToInt = parseInt(moves);
+		let fullStars = document.getElementsByClassName(FULL_STAR)[0];
+		let halfStars = document.getElementsByClassName(STAR_HALF_FULL)[0];
+		if (movesToInt % 4 === 2 && fullStars != undefined) {
+			let elem = document.getElementsByClassName(FULL_STAR)[0];
+			elem.classList.remove(FULL_STAR);
+			elem.classList.add(STAR_HALF_FULL);
+		} else if (movesToInt % 8 === 2 && halfStars != undefined) {
+			let elem = document.getElementsByClassName(STAR_HALF_FULL)[0];
+			elem.classList.remove(STAR_HALF_FULL);
+		}
+	},
+	doRestartGame: function() {
+		document.querySelectorAll(RESTART).forEach((e) => {
+      e.style.display='block';
+			e.addEventListener('click', function() {
+				restart();
+				resetMovesToZero();
+			});
+		});
 
-    function resetMovesToZero() {
-      document.getElementsByClassName(MOVES)[0].textContent = "0";
-    }
-    function restart() {
-      document.querySelectorAll("li").forEach(el => {
-        el.classList.remove(MATCH);
-      });
-    }
-  }
+		function resetMovesToZero() {
+			document.getElementsByClassName(MOVES)[0].textContent = '0';
+		}
+		function restart() {
+			document.querySelectorAll('li').forEach((el) => {
+				el.classList.remove(MATCH);
+			});
+		}
+	}
 };
 
 MemoryGame.DISPLAY = {
-  doShuffle: function() {
-    MemoryGame.deck.innerHTML = "";
+	doShuffle: function() {
+		MemoryGame.deck.innerHTML = '';
 
-    let copyOfCards = [...MemoryGame.cards];
-    shuffle(copyOfCards).forEach(e => MemoryGame.deck.appendChild(e));
-    /*
+		let copyOfCards = [ ...MemoryGame.cards ];
+		shuffle(copyOfCards).forEach((e) => MemoryGame.deck.appendChild(e));
+		/*
     * Display the cards on the page
     *   - shuffle the list of cards using the provided "shuffle" method below
     *   - loop through each card and create its HTML
     *   - add each card's HTML to the page
     */
 
-    // Shuffle function from http://stackoverflow.com/a/2450976
-    function shuffle(array) {
-      var currentIndex = array.length,
-        temporaryValue,
-        randomIndex;
+		// Shuffle function from http://stackoverflow.com/a/2450976
+		function shuffle(array) {
+			var currentIndex = array.length,
+				temporaryValue,
+				randomIndex;
 
-      while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
+			while (currentIndex !== 0) {
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex -= 1;
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+			}
 
-      return array;
-    }
-  },
-  doMatch: function(currentElement) {
-    let pairedElements = MemoryGame.pairedArray.filter(
-      i =>
-        i.firstElementChild.classList.value ===
-        currentElement.firstElementChild.classList.value
-    );
+			return array;
+		}
+	},
+	doMatch: function(currentElement) {
+		let pairedElements = MemoryGame.pairedArray.filter(
+			(i) => i.firstElementChild.classList.value === currentElement.firstElementChild.classList.value
+		);
 
-    //track moves
-    MemoryGame.PLAY.trackMoves();
-    //track star
-    MemoryGame.PLAY.reduceStar();
+		//track moves
+		MemoryGame.PLAY.trackMoves();
+		//track star
+		MemoryGame.PLAY.reduceStar();
 
-    //if not the first time and cards do not match remove them from DOM and hide from UI
-    if (pairedElements.length === 1 && MemoryGame.pairedArray.length === 2) {
-      MemoryGame.pairedArray.forEach(el => {
-        setTimeout(function() {
-          el.classList.remove(OPEN, SHOW);
-        }, MemoryGame.timeoutInMs);
-      });
-      MemoryGame.pairedArray = [];
-    } else if (
-      MemoryGame.pairedArray.length === 2 &&
-      pairedElements.length === 2
-    ) {
-      MemoryGame.pairedArray.forEach(el => {
-        el.classList.remove(OPEN, SHOW);
-        el.classList.add(MATCH);
-      });
-      MemoryGame.pairedArray = [];
-    }
-  },
-  showModelIfApplicable: function() {
-    let matchedCardCount = document.querySelectorAll(`.${CARD}.${MATCH}`)
-      .length;
-    if (matchedCardCount === 16) {
-        let move = document.getElementsByClassName(MOVES)[0].textContent;
-        let stars = document.getElementsByClassName(FULL_STAR).length;
-      //replace with modal
-      window.alert(
-        `You have succesfully completed the game with ${move} moves and ${stars} stars.`
-      );
-    }
-  }
+		//if not the first time and cards do not match remove them from DOM and hide from UI
+		if (pairedElements.length === 1 && MemoryGame.pairedArray.length === 2) {
+			MemoryGame.pairedArray.forEach((el) => {
+				setTimeout(function() {
+					el.classList.remove(OPEN, SHOW);
+				}, MemoryGame.timeoutInMs);
+			});
+			MemoryGame.pairedArray = [];
+		} else if (MemoryGame.pairedArray.length === 2 && pairedElements.length === 2) {
+			MemoryGame.pairedArray.forEach((el) => {
+				el.classList.remove(OPEN, SHOW);
+				el.classList.add(MATCH);
+			});
+			MemoryGame.pairedArray = [];
+		}
+	},
+	showModelIfApplicable: function() {
+		let matchedCardCount = document.querySelectorAll(`.${CARD}.${MATCH}`).length;
+		if (matchedCardCount === 16) MemoryGame.MODAL.displayModal();
+	}
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-  MemoryGame.PLAY.initializeVariables();
-  MemoryGame.DISPLAY.doShuffle();
-  MemoryGame.PLAY.doPlay();
-  MemoryGame.PLAY.doRestartGame();
+MemoryGame.MODAL = {
+	displayModal: function() {
+		let move = document.getElementsByClassName(MOVES)[0].textContent;
+		let stars = document.getElementsByClassName(FULL_STAR).length;
+		//replace with modal
+		//window.alert(`You have succesfully completed the game with ${move} moves and ${stars} stars.`);
+		document.getElementById('modal').style.display = 'block';
+		document.getElementById(
+			'contentText'
+		).textContent = `You have succesfully completed the game with ${move} moves and ${stars} stars.`;
+	}
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+	MemoryGame.PLAY.initializeVariables();
+	MemoryGame.DISPLAY.doShuffle();
+	MemoryGame.PLAY.doPlay();
+	MemoryGame.PLAY.doRestartGame();
 });
 
 /*
